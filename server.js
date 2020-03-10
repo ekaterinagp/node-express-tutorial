7840027f9ccb9346e1a2f38909a89e54f52a67bf
ekaterinagp <katya.koriakina@gmail.com>
2020-03-10 18:27:29 +0100

    dynamic routes

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+const cookieSession = require("cookie-session");
+
+const FeedbackService = require("./services/FeedbackService");
+const SpeakersService = require("./services/SpeakerService");
+
+const feedbackService = new FeedbackService("./data/feedback.json");
+const speakersService = new SpeakersService("./data/speakers.json");
+
+const routes = require("./routes");
+
+const app = express();
+
+const port = 3000;
+
+app.set("trust proxy", 1); //if has to deploy with cookies, iotherwise will not work
+
+app.use(
+  cookieSession({
+    name: "session",
+    keys: ["vcjskdsdksld", "sdhsjdskdsd"]
+  })
+);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "./views"));
+
+app.use(express.static(path.join(__dirname, "./static")));
+
+app.use(
+  "/",
+  routes({
+    feedbackService,
+    speakersService
+  })
+);
+
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}!`);
+});
